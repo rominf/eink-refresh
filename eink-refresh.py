8565cbc0e77a7b7be291450162c5a46d2cc929c6
Roman Inflianskas <infroma@gmail.com>
2015-06-06 14:03:02 +0300

    program initial

---
+++ eink-refresh.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""
+Refresh e-ink display.
+
+Usage:
+    eink-refresh.py [--refresh-delay=<delay>]
+
+Options:
+    --refresh-delay=<delay>  Time needed to change color from black to white [Default: 600.0].
+"""
+
+import mainwindow_ui
+
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+from docopt import docopt
+
+from time import sleep
+import sys
+
+
+class MainWindow(QWidget):
+    def __init__(self, processEvents, refresh_delay):
+        super(MainWindow, self).__init__()
+        self.refresh_delay = refresh_delay
+        self.processEvents = processEvents
+        self.ui = mainwindow_ui.Ui_Form()
+        self.ui.setupUi(self)
+
+    def sleep(self, ms):
+        self.processEvents()
+        sleep(ms/1000.0)
+
+    def refresh_and_close(self):
+        self.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.sleep(self.refresh_delay)
+        self.close()
+
+    def showEvent(self, QShowEvent):
+        QTimer.singleShot(self.refresh_delay, self.refresh_and_close)
+
+
+def main(args):
+    app = QApplication(sys.argv)
+    refresh_delay = float(args['--refresh-delay'])
+    main_window = MainWindow(
+        processEvents=app.processEvents,
+        refresh_delay=refresh_delay,
+    )
+    main_window.showFullScreen()
+    return app.exec_()
+
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+    sys.exit(main(args))
---
+++ mainwindow_ui.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.3.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(400, 300)
+        Form.setWindowTitle("")
+        Form.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"background-color: rgb(0, 0, 0);")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        pass
+
